42aad52d52ccb7215e7c59463ecd099c9c5cab81
caas23 <caas23@student.bth.se>
2024-12-21 20:14:45 +0100

    Adapt to Solo Scoot

--- services/user-web-ui/src/components/Footer/index.tsx
+++ services/user-web-ui/src/components/Footer/index.tsx
@@ -5,30 +5,8 @@ const Footer: React.FC = () => {
 	return (
 		<footer className="footer">
 			<div className="footer-wrapper">
-				<ul>
-					<li>
-						<a href="ride-history">Some link</a>
-					</li>
-					<li>
-						<a href="profile">Another one</a>
-					</li>
-					<li>
-						<a href="payment">Here goes a third link</a>
-					</li>
-				</ul>
-				<ul>
-					<li>
-						<a href="ride-history">Scooter</a>
-					</li>
-					<li>
-						<a href="profile">About Solo Scoot</a>
-					</li>
-					<li>
-						<a href="payment">Manage payments</a>
-					</li>
-				</ul>
 			</div>
-            <p>&copy; Copyright 2024 – Solo Scoot, Inc.</p>
+            <p>&copy; Copyright 2024 – Solo Scoot</p>
 		</footer>
 	);
 };
