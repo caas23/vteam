c83fe7ee06cc3510bf518ecc122e965e3630d997
caas23 <caas23@student.bth.se>
2024-12-22 21:09:21 +0100

    Udated fetch models to work with backend auth check

--- services/user-web-ui/src/fetchModels/fetchAddUser.tsx
+++ services/user-web-ui/src/fetchModels/fetchAddUser.tsx
@@ -4,15 +4,22 @@ export const fetchAddUser = async (userData: object) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        'Authorization': `Bearer ${sessionStorage.getItem('access_token')}`,
       },
       body: JSON.stringify(userData),
     });
 
-    if (!response.ok) {
-      throw new Error("Failed to add user");
+    if (response.status === 401) {
+      window.location.href = '/';
+      return;
     }
-
+  
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+  
     return await response.json();
+
   } catch (error) {
     console.error("Error adding new user:", error);
     return null;
--- services/user-web-ui/src/fetchModels/fetchOneUser.tsx
+++ services/user-web-ui/src/fetchModels/fetchOneUser.tsx
@@ -1,10 +1,23 @@
 export const fetchOneUserByGitId = async (id: number) => {
   try {
-    const response = await fetch(`http://localhost:1337/get/one/git/user/?id=${id}`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch user data");
+    const response = await fetch(`http://localhost:1337/get/one/git/user/?id=${id}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${sessionStorage.getItem('access_token')}`,
+      },
+    });
+   
+    if (response.status === 401) {
+      window.location.href = '/';
+      return;
     }
+  
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+  
     return await response.json();
+
   } catch (error) {
     console.error(`Error fetching user with id ${id}:`, error);
     return null;
