b895417b9eb07b4df5b8f00147ff7434ac1fd37c
caas23 <caas23@student.bth.se>
2025-01-03 23:55:55 +0100

    Update inserted trips with route coordinates

--- services/admin-web-ui/src/components/Trip/interfaces.tsx
+++ services/admin-web-ui/src/components/Trip/interfaces.tsx
@@ -5,6 +5,9 @@ interface Trip {
   start_location: [number, number];
   end_location: [number, number];
   price: number;
+  route: [number, number][];
+  distance: number;
+  trip_id: string;
 }
 
 interface TripDetailsProps {
@@ -12,6 +15,7 @@ interface TripDetailsProps {
 }
 
 interface TripMapProps {
+  data: Trip;
   startLocation: [number, number];
   endLocation: [number, number];
   FetchedDistance?: (distance: number) => void;
