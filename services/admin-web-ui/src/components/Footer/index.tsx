d5da5b009a4835f0ad60793d40fcf5ddd2387355
caas23 <caas23@student.bth.se>
2025-01-20 02:40:44 +0100

    Updated footer

--- services/admin-web-ui/src/components/Footer/index.tsx
+++ services/admin-web-ui/src/components/Footer/index.tsx
@@ -4,31 +4,7 @@ import "./index.css";
 const Footer: React.FC = () => {
 	return (
 		<footer className="footer">
-			<div className="footer-wrapper">
-				{/* <ul>
-					<li>
-						<a href="">Some link</a>
-					</li>
-					<li>
-						<a href="">Another one</a>
-					</li>
-					<li>
-						<a href="">Here goes a third link</a>
-					</li>
-				</ul>
-				<ul>
-					<li>
-						<a href="">Scooter</a>
-					</li>
-					<li>
-						<a href="">About Solo Scoot</a>
-					</li>
-					<li>
-						<a href="">Manage payments</a>
-					</li>
-				</ul> */}
-			</div>
-            <p>&copy; Copyright 2024 – Solo Scoot</p>
+            <p>- - - Solo Scoot - - -</p>
 		</footer>
 	);
 };
