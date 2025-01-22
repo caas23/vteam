6ce7f8660a19ef748e92dcfadd324878aea5f81f
caas23 <caas23@student.bth.se>
2025-01-22 14:01:52 +0100

    Changed lt to ngrok and added reset option

--- simulation/system_up.bash
+++ simulation/system_up.bash
@@ -1,16 +1,40 @@
 #!/bin/bash
 
+# run --reset to reset db before starting system
+
+RESET_DB_SCRIPT="../services/db/scripts/resetDb.bash"
+
+reset_database() {
+    if [[ -f "$RESET_DB_SCRIPT" ]]; then
+        bash "$RESET_DB_SCRIPT"
+        if [[ $? -ne 0 ]]; then
+            exit 1
+        fi
+    else
+        echo "Reset script not found, invalid path '$RESET_DB_SCRIPT'"
+        exit 1
+    fi
+}
+
+if [[ "$1" == "--reset" ]]; then
+    reset_database
+fi
+
 docker-compose up --build -d
 
 clear
 echo "Starting system..."
 sleep 10
 
-lt -s --port 1337 --subdomain vteambackend &
-# Show QR code (comment out if the app is already linked to Expo Go)
+# silence output but save PID for termination later
+ngrok http --url=wasp-romantic-crow.ngrok-free.app 1337 > /dev/null &
+NGROK_PID=$!
+echo "ngrok process started"
+
+# show QR code (comment out if the app is already linked to Expo Go)
 docker logs -f user-app | awk '/Tunnel ready\./, /› Metro waiting/ {line++; if (line >= 2 && line <= 18) print $0}' &
 
-# Make sure QR code comes first in the terminal output
+# make sure QR code comes first in the terminal output
 sleep 5
 
 echo "========================================================="
@@ -22,7 +46,12 @@ echo "========================================================="
 while true; do
     read -p "Type 'exit' to stop the system: " user_input
     if [[ "$user_input" == "exit" ]]; then
-        pkill -f "lt --port 1337 --subdomain vteambackend"
+        if [[ -n "$NGROK_PID" ]] && kill -0 "$NGROK_PID" 2>/dev/null; then
+            kill "$NGROK_PID"
+            echo "ngrok process stopped"
+        else
+            echo "No active ngrok process found."
+        fi
         
         docker-compose down
     
